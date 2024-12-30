be8d819a5719b02bfad0ac83ef6779a847833a21
Seafarer <willie77092@gmail.comm>
2024-12-30 23:45:18 +0800

    修正checkblance方法及測試

--- shopping/shoppingCart.py
+++ shopping/shoppingCart.py
@@ -44,11 +44,12 @@ class ShoppingCart:
                 self.shopping_cart.clear()
                 print(f"\n付款成功！剩餘餘額: ${self.user_balance}")
 
-    def checkBalance(self):
-         if self.user_balance > 0:
-             print(f"\n剩餘餘額: ${self.user_balance}")
-         else:
-             print("\n餘額不足")
+    def checkBalance(self, total, paid):
+        balance = paid - total
+        if balance >= 0:
+            return balance
+        else:
+            return "餘額不足"
 
 if __name__ == '__main__':
     cart = ShoppingCart()
--- tests/unit/test_check_balance.py
+++ tests/unit/test_check_balance.py
@@ -8,10 +8,10 @@ class TestShoppingCart(unittest.TestCase):
 
     # 測試檢查餘額功能是否是否如同預期執行
     def test_check_balance(self):
-         cart = self.cart
-         self.assertEqual(cart.checkBalance(100, 50), 50)
-         self.assertEqual(cart.checkBalance(200, 100), 100)
-         self.assertEqual(cart.checkBalance(50, 100), "餘額不足")
+        cart = ShoppingCart()  # 建立 ShoppingCart 實例
+        self.assertEqual(cart.checkBalance(100, 50), "餘額不足")  # 測試不足的餘額
+        self.assertEqual(cart.checkBalance(100, 150), 50)        # 測試足夠的餘額
+        self.assertEqual(cart.checkBalance(200, 200), 0)         # 測試剛好支付的情況
 
 if __name__ == '__main__':
     unittest.main()
